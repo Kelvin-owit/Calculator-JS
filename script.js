2c9f99baf3fbe6af3f2ce2d33102401a31797ddd
Kelvin-owit <owitkelvin@gmail.com>
2023-01-25 20:14:55 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// var display = document.getElementById("output-screen");
+// function display(num) {
+//   display.value += num;
+// }
+// var display = (document.getElementById("output-screen").onclick =
+//   display.value);
